0a07ce6e69c16a7b14531c8f591912a9347cc146
MEetpanchal1222 <panchalmeetr97@gmail.com>
2024-02-19 17:53:31 +0530

    19/2/23   👌

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -27,77 +27,90 @@ class _homescreenState extends State<homescreen> {
           body: SingleChildScrollView(
             child: Column(
             children: [
-             Row(
-               children: [
-                 SizedBox(width: 40,),
-                     Image(image:AssetImage('assest/homescreen/Group 3.png')),
-                 SizedBox(width: 190,),
-                 Text('New Arivales',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
-                 SizedBox(width: 10,),
-                 Text('Men',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
-                 SizedBox(width: 10,),
-                 Text('Women',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
-                 SizedBox(width: 10,),
-                 Text('kides',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
-                 SizedBox(width: 140,),
-                 Image(image:AssetImage('assest/homescreen/icons/fi_shopping-cart.png')),
-                 SizedBox(width: 20,),
-                 Image(image:AssetImage('assest/homescreen/icons/Vector.png')),
-                 SizedBox(width: 20,),
-                 Image(image:AssetImage('assest/homescreen/icons/fi_user.png')),
-                        ],
-                     ),
+             SingleChildScrollView(
+               scrollDirection: Axis.horizontal,
+               child: Row(
+                 children: [
+                   SizedBox(width: 40,),
+                       Image(image:AssetImage('assest/homescreen/Group 3.png')),
+                   SizedBox(width: 490,),
+                   Text('New Arivales',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
+                   SizedBox(width: 30,),
+                   Text('Men',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
+                   SizedBox(width: 30,),
+                   Text('Women',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
+                   SizedBox(width: 30,),
+                   Text('kides',style: TextStyle(fontSize: 15,color: Colors.black87,fontWeight: FontWeight.bold),),
+                   SizedBox(width: 490,),
+                   Image(image:AssetImage('assest/homescreen/icons/fi_shopping-cart.png')),
+                   SizedBox(width: 20,),
+                   Image(image:AssetImage('assest/homescreen/icons/Vector.png')),
+                   SizedBox(width: 20,),
+                   Image(image:AssetImage('assest/homescreen/icons/fi_user.png')),
+                          ],
+                       ),
+             ),
               Divider(height: 0.5,thickness: 0.5,color: Colors.black87),
               SizedBox(height: 100,),
-              Row(
-                  children: [
-                    SizedBox(width: 110,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                    Text('Puma',style: TextStyle(fontSize: 50,color: Colors.black87,fontWeight: FontWeight.bold),),
-                  Text('Runiing SX\n',style: TextStyle(fontSize: 50,color: Colors.black87,fontWeight: FontWeight.bold,),),
-                         // SizedBox(height: 30,width: 10,),
-                         Image(image: AssetImage('assest/homescreen/textd1.png'),width: 250,),
-                          SizedBox(height: 20,),
-                          Text('62,000 RwF',style: TextStyle(fontWeight: FontWeight.bold),),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            height: 30,width: 80,
-                            decoration: BoxDecoration(
-                              color: colormeet,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                    children: [
+                      SizedBox(width: 110,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                      Text('Puma',style: TextStyle(fontSize: 50,color: Colors.black87,fontWeight: FontWeight.bold),),
+                    Text('Runiing SX\n',style: TextStyle(fontSize: 50,color: Colors.black87,fontWeight: FontWeight.bold,),),
+                           // SizedBox(height: 30,width: 10,),
+                           Image(image: AssetImage('assest/homescreen/textd1.png'),width: 250,),
+                            SizedBox(height: 20,),
+                            Text('62,000 RwF',style: TextStyle(fontWeight: FontWeight.bold),),
+                            SizedBox(
+                              height: 10,
                             ),
-                            child: Center(child: Text('Add to cart',style: TextStyle(color: Colors.white,fontSize: 10),)),
-                          )
+                            Container(
+                              height: 30,width: 80,
+                              decoration: BoxDecoration(
+                                color: colormeet,
+                              ),
+                              child: Center(child: Text('Add to cart',style: TextStyle(color: Colors.white,fontSize: 10),)),
+                            )
 
-                ]
-             ),
-               SizedBox(width: 300,),
-                Column(
-                children: [
-                  Container(
-                    height: 350,
-                    width: 300,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0XFFf1eaef),
-                    ),
-                    child: Image(image: AssetImage('assest/homescreen/Rectangle 4.png'),height: 350,width: 300,) ,
+                  ]
+                             ),
+                 SizedBox(width: 300,),
+                  Column(
+                  children: [
+                    Container(
+                      height: 350,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0XFFf1eaef),
+                        boxShadow: [
+                        BoxShadow(
+                        spreadRadius: 1,
+                        color: Colors.black87,
+                        blurRadius: 20,
+                      ),
+                    ],
                   ),
+                      child: Image(image: AssetImage('assest/homescreen/Rectangle 4.png'),height: 350,width: 300,) ,
+                    ),
 
+                  ],
+                ),
                 ],
+                            ),
               ),
-              ],
-            ),
               Divider(height: 1,thickness: 1.3,color: Colors.grey),
               SizedBox(height: 20,),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
-                    SizedBox(width: 110,),
+                    SizedBox(width: 25,),
                     Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
